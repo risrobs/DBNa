a79b57a641431b962c6c22905b05547cc7caa93f
risrobs <robertivanov22@abv.bg>
2021-12-18 16:13:13 +0200

    koti3.0

--- src/js/app.js
+++ src/js/app.js
@@ -8,16 +8,4 @@ window.addEventListener("DOMContentLoaded", () => {
     alert("💣");
   });
 });
- img = document.getElementById("img1");
-          
-            function enlargeImg() {
-                img.style.width = "60%";
-                img.style.height = "auto";
-                img.style.transition = "width 0.5s ease";
-            }
-            
-            function resetImg() {
-                img.style.width = "40%";
-                img.style.height = "auto";
-                img.style.transition = "width 0.5s ease";
-            }
+
